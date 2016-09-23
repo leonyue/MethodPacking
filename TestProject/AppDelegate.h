acadb53969938a668a3187b6cf9d1c8a53d7f387
Leon Yue <4940748@qq.com>
2016-09-23 16:03:56 +0800

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by YC-JG-YXKF-PC35 on 16/9/23.
+//  Copyright © 2016年 YC-JG-YXKF-PC35. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by YC-JG-YXKF-PC35 on 16/9/23.
+//  Copyright © 2016年 YC-JG-YXKF-PC35. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
